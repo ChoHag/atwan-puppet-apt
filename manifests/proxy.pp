c129950777e1c21f14519772b42ca602e3932837
Matthew King <matthew.king@monnsta.net>
2013-07-15 10:52:11 +0100

    Removal of proxy settings file.

--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -1,6 +1,12 @@
 class apt::proxy ($proxy_url) {
-  file { "/etc/apt/apt.conf.d/proxy":
-    ensure  => file,
-    content => template('apt/apt_conf_proxy.erb');
+  if $proxy_url != '' {
+    file { "/etc/apt/apt.conf.d/proxy":
+      ensure  => file,
+      content => template('apt/apt_conf_proxy.erb');
+    }
+  } else {
+    file { "/etc/apt/apt.conf.d/proxy":
+      ensure  => absent;
+    }
   }
 }
